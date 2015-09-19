4085e316b39f63300eb4106e500b0004c4020d55
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2015-09-19 08:41:53 +0200

    JPEGOptim

--- images/poptimize.sh
+++ images/poptimize.sh
@@ -5,6 +5,10 @@
 if [ "$2" = "1"  ]; then
 	BASENAME="${1%.png}"
 	optipng -o7 "${BASENAME}.png"
-else
+elif [ "$2" = "2"  ]; then
+	BASENAME="${1%.jpg}"
+	jpegoptim "${BASENAME}.jpg"
+else 
 	find  . -name "*.png" | parallel ./poptimize.sh "{}" 1
+	find  . -name "*.jpg" | parallel ./poptimize.sh "{}" 2
 fi
